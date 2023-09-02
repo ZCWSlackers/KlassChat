feae54e28f1e31cadcb771e29f90c17d57d16921
Liz Kinker <Liz.Kinker@gmail.com>
2023-09-02 18:06:55 -0400

    Channel Buttons

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -4,7 +4,7 @@ function loadChannelNames() {
   fetch(`${API_URL}/api/channels`)
     .then(response => response.json())
     .then(data => {
-      showChannelNames(data);
+      createChannelButtons(data);
     })
     .catch(error => {
       console.error('Error fetching channel data: ', error);
@@ -23,3 +23,40 @@ function showChannelNames(data) {
 }
 
 window.addEventListener('load', loadChannelNames);
+
+var coll = document.getElementsByClassName('collapsible');
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var content = this.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+}
+
+function createChannelButtons(data) {
+  const channelButtons = document.getElementById('channelList');
+
+  data.forEach(channel => {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.textContent = channel.name;
+    button.addEventListener('click', function () {
+      handleChannelButtonClick(channel.name);
+    });
+
+    const li = document.createElement('li');
+    li.appendChild(button);
+    channelButtons.appendChild(li);
+  });
+}
+
+function handleChannelButtonClick(channelName) {
+  const channelNameElement = document.querySelector('.channel-name');
+  channelNameElement.textContent = channelName;
+}
