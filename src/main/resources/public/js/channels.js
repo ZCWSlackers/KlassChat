4885cadcc956b26b52062c9049a98feddd19376d
Liz Kinker <Liz.Kinker@gmail.com>
2023-09-07 12:54:20 -0400

    Save it before breaking it

--- src/main/resources/public/js/channels.js
+++ src/main/resources/public/js/channels.js
@@ -23,7 +23,7 @@ let coll = document.getElementsByClassName('collapsible');
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener('click', function () {
     this.classList.toggle('active');
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === 'block') {
       content.style.display = 'none';
     } else {
--- src/main/resources/public/js/workspaces.js
+++ src/main/resources/public/js/workspaces.js
@@ -1,16 +1,34 @@
 import { API_URL } from './constants.js';
 
+let workspaceId;
+let workspaceData = [];
+
 function fetchWorkspaceData() {
   fetch(`${API_URL}/api/workspaces`)
     .then(response => response.json())
     .then(data => {
-      createChannelDropdown(data);
+      workspaceData = data;
+      createWorkspaceButtons(data);
     })
     .catch(error => {
       console.error('Error fetching channel data: ', error);
     });
 }
+window.addEventListener('load', fetchWorkspaceData);
 
-function createChannelDropdown(workspaceData) {
-  const selectedChannel = document.createElement('selected-channel');
+function createWorkspaceButtons(data, workspaceDropdown) {
+  // const workspaceDropdown = document.getElementById('workspaceDropdown');
+  workspaceDropdown.innerHTML = '';
+
+  data.forEach(workspace => {
+    const option = document.createElement('option');
+    option.value = workspace.id;
+    option.textContent = workspace.name;
+    workspaceDropdown.appendChild(option);
+  });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DomContentLoaded event');
+  fetchWorkspaceData();
+});
