a3f8dc4ecdebe6df95bc19c6a2d13e3e0d3c6171
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-09-13 08:20:30 -0400

    urls docker ready

--- src/main/resources/public/js/constants.js
+++ src/main/resources/public/js/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = `http://localhost:8080`;
+//export const API_URL = `http://localhost:8080`;
 // const API_URL = `http://192.168.3.39:8080`;
-//export const API_URL = `https://klasschat.zipcode.rocks`;
+export const API_URL = `https://klasschat.zipcode.rocks`;
 // needs to be 8087 for online
